67e2560c81e27df3a01713ff368dd141c0a9b400
Shulamit Ajzner <shulajz@hotmail.com>
2019-02-16 19:05:05 +0200

    testing1

--- src/pages/login/login-ionic.ts
+++ src/pages/login/login-ionic.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'page-hello-ionic',
   templateUrl: 'login-ionic.html'
 })
 export class LoginIonicPage {
+
+  @ViewChild('username') uname;
+  @ViewChild('password') password;
+
+
+
   constructor() {
 
+  }
+
+  signIn(){
+    // alert("hi! your name is: " + this.uname.value + "\nand your password is: " +  this.password.value);
+    if((!this.uname.value) || (!this.uname.password)){
+      alert("לא הזנת את כל הנתונים")
+    }
   }
 }
