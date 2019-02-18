933aab045837bf1b9863ffac96acd51b42ce1242
or shaashua <orshaashua@gmail.com>
2019-02-18 16:29:46 +0200

    settings

--- src/pages/settings/settings.ts
+++ src/pages/settings/settings.ts
@@ -1,5 +1,6 @@
 import {Component, ViewChild} from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 
 /**
  * Generated class for the SettingsPage page.
@@ -19,18 +20,18 @@ export class SettingsPage {
   @ViewChild('gender') gender;
   @ViewChild('BirthDate') BirthDate;
   @ViewChild('mentor') mentor;
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams){
+    
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad SettingsPage');
   }
-  doneSettings(){
 
+  doneSettings(){
     alert("the addicts type is: " + this.addictsType.value + "\n"
       + "the gender: " +this.gender.value+ "\n"
       + "the Birth Date: year: "  + this.BirthDate.value.year+ " mounth: "+this.BirthDate.value.month + " day: "+this.BirthDate.value.day +"\n"
       +"mentor? " +this.mentor.value);
   }
-
 }
