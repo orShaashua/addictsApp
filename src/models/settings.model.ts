2155b0c58b75ce745aecfe4343a24e7698f1a973
shulajz <shulajz@gmail.com>
2019-05-21 13:30:11 +0300

    class settings

---
+++ src/models/settings.model.ts
@@ -0,0 +1,25 @@
+export class Settings {
+
+  //Fields
+  gender: string;
+  addictsType: string;
+  mentor: boolean;
+  bdayYear: number;
+  bdayMonth: number;
+  bdayDay: number;
+  about: string;
+
+  constructor (gender: string, addictsType: string,
+               mentor: boolean,
+               bdayYear: number,
+               bdayMonth: number,
+               bdayDay: number,
+               about: string){
+    this.gender = gender;
+    this.addictsType = addictsType;
+    this.mentor = mentor;
+    this.bdayYear = bdayYear;
+    this.bdayMonth = bdayMonth;
+    this.bdayDay = bdayDay;
+    this.about = about;
+  }}
