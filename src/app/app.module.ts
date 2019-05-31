437f80954103494cd0077e94142eacb58f33fca1
or shaashua <orshaashua@gmail.com>
2019-05-31 13:36:56 +0300

    design for the login page and for the register page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -34,7 +34,6 @@ import { SwipeCardsModule } from 'ng2-swipe-cards';
 import {SearchFriendsPage} from "../pages/search-friends/search-friends";
 import {MatchPage} from "../pages/match/match";
 import {FiltersService} from "../services/FiltersService";
-import {Push} from "@ionic-native/push/ngx";
 
 var config = {
   apiKey: "AIzaSyCHyiRzPEQKu03pF9bny8CZ-p6B1CdsJ5o",
@@ -95,7 +94,6 @@ var config = {
     File,
     FilePath,
     FileChooser,
-    Push,
     {provide: ErrorHandler, useClass: IonicErrorHandler},
     AuthProvider,
     UserProvider,
